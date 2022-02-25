6412ab16ade81d12dc08ef3875c9421e9bf663c8
Thomas Bischoff <thomascbischoff@gmail.com>
2022-02-24 20:02:17 -0500

    Add Solution to 04_number_compare

--- 04_number_compare/number_compare.py
+++ 04_number_compare/number_compare.py
@@ -10,3 +10,9 @@ def number_compare(a, b):
         >>> number_compare(1, -2)
         'First is greater'
     """
+    if a == b:
+        print("Numbers are Equal")
+    elif a < b:
+        print("Second is Greater")
+    else:
+        print("First is Greater")
