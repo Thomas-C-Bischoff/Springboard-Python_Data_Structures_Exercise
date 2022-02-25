5caaef05791204e7b7cd293606c691540c69255c
Thomas Bischoff <thomascbischoff@gmail.com>
2022-02-25 12:58:19 -0500

    Add Solution to 07_multiple_letter_count

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,10 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    letter_count = {}
+    for char in phrase:
+        if char in letter_count.keys():
+            letter_count[char] = letter_count[char] + 1
+        else:
+            letter_count[char] = 1
+    return letter_count
