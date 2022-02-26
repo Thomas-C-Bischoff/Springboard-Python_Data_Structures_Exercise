91cd665a88d44b90d97fa05db4d1c6985a3bb7a4
Thomas Bischoff <thomascbischoff@gmail.com>
2022-02-25 19:05:35 -0500

    Add Solution to 14_compact

--- 14_compact/compact.py
+++ 14_compact/compact.py
@@ -4,3 +4,8 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
+    copy = []
+    for value in lst:
+        if value:
+            copy.append(value)
+    return copy
